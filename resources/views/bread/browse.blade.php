2ab7c0895e70dfd4d85940ed9893e7a2de0056bf
souravdas25 <souravbumbadas25@gmail.com>
2018-06-28 20:46:47 +0530

    v1.2.3

--- resources/views/bread/browse.blade.php
+++ resources/views/bread/browse.blade.php
@@ -24,6 +24,19 @@
         @endcan
         @include('voyager::multilingual.language-selector')
     </div>
+    <style>
+        .select2-container {
+            min-width : 100px !important;
+            width : 100%;
+        }
+        #search-input{
+            border : 0;
+        }
+
+        #search-input input {
+            border: 1px black !important;
+        }
+    </style>
 @stop
 
 @section('content')
@@ -34,24 +47,28 @@
                 <div class="panel panel-bordered">
                     <div class="panel-body">
                         @if ($isServerSide)
-                            <form method="get" class="form-search">
-                                <div id="search-input">
-                                    <select id="search_key" name="key">
-                                        @foreach($searchable as $key)
+                            <form method="get" class="form-search ">
+                                <div id="search-input" class="form-row">
+                                    <div class="col-sm-2">
+                                        <select id="search_key" name="key" style="min-width: 50px !important;width:100%" >
+                                            @foreach($searchable as $key)
                                                 <option value="{{ $key }}" @if($search->key == $key){{ 'selected' }}@endif>{{ ucwords(str_replace('_', ' ', $key)) }}</option>
-                                        @endforeach
-                                    </select>
-                                    <select id="filter" name="filter">
-                                        <option value="contains" @if($search->filter == "contains"){{ 'selected' }}@endif>contains</option>
-                                        <option value="equals" @if($search->filter == "equals"){{ 'selected' }}@endif>=</option>
-                                    </select>
-                                    <div class="input-group col-md-12">
-                                        <input type="text" class="form-control" placeholder="{{ __('voyager::generic.search') }}" name="s" value="{{ $search->value }}">
-                                        <span class="input-group-btn">
-                                            <button class="btn btn-info btn-lg" type="submit">
-                                                <i class="voyager-search"></i>
-                                            </button>
-                                        </span>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                    <div class="col-sm-2">
+                                        <select id="filter" name="filter" class="col-sm-2" style="min-width: 50px !important;width:100%">
+                                            <option value="contains" @if($search->filter == "contains"){{ 'selected' }}@endif>contains</option>
+                                            <option value="equals" @if($search->filter == "equals"){{ 'selected' }}@endif>=</option>
+                                        </select>
+                                    </div>
+                                    <div class=" col-sm-8" >
+                                        <input type="text" class="col-sm-8 pb-2 grey lighten-4"
+                                               placeholder="{{ __('voyager::generic.search') }}"
+                                               name="s" value="{{ $search->value }}">
+                                        <button class="btn btn-info btn-lg" type="submit">
+                                            <i class="voyager-search"></i>
+                                        </button>
                                     </div>
                                 </div>
                             </form>
